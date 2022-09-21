27f6de156e94d93309ec038fd414bd86922905f8
Christian Jose <christianjsanchezr2025@gmail.com>
2022-09-20 21:45:09 -0400

    Implementando WebSocket

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,16 @@
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { MessagesWsService } from './messages-ws.service';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+  
+  handleConnection(client: Socket, ...args: any[]) {
+    this.messagesWsService.registerClient(client);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client);
+  }
+}
